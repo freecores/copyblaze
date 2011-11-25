188c7b99a453d1da8e3b83d4f031cef06ff5aca8
ameziti <ameziti@3e6cdf6f-a36d-4301-a5d4-f7dc43255b80>
2011-11-25 10:06:20 +0000

    SW:   - wishbone :     - add asm code for the "wb_timer_08" wishbone validation test     - add asm code for the "wb_uart_08" wishbone validation test

---
+++ copyblaze/sw/code/pBlaze/wb_timer/wb_timer.asm
@@ -0,0 +1,19 @@
+; project	: copyBlaze 8 bit processor
+; file name	: wb_timer.asm
+; author	: abdAllah Meziti
+; licence	: LGPL
+
+; this programm test the wishbone copyBlaze instruction.
+; it use this module : 
+; 			wb_timer_08.vhd
+
+		;
+
+		; ==========================================================
+start:
+		; ==========================================================
+
+
+end:		
+		JUMP	end
+		;
---
+++ copyblaze/sw/code/pBlaze/wb_uart/wb_uart.asm
@@ -0,0 +1,19 @@
+; project	: copyBlaze 8 bit processor
+; file name	: wb_uart.asm
+; author	: abdAllah Meziti
+; licence	: LGPL
+
+; this programm test the wishbone copyBlaze instruction.
+; it use this module : 
+; 			wb_uart_08.vhd
+
+		;
+
+		; ==========================================================
+start:
+		; ==========================================================
+
+
+end:		
+		JUMP	end
+		;
