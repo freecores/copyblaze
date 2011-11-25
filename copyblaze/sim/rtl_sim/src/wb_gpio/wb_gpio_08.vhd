f9f08ee2b0a1a18642d7f5f8acb3de2b7fce625a
ameziti <ameziti@3e6cdf6f-a36d-4301-a5d4-f7dc43255b80>
2011-11-25 09:58:02 +0000

    SIM:   - wishbone :     - add constants value of the address of wg_gpio_08 internal registers

--- copyblaze/sim/rtl_sim/src/wb_gpio/wb_gpio_08.vhd
+++ copyblaze/sim/rtl_sim/src/wb_gpio/wb_gpio_08.vhd
@@ -26,6 +26,9 @@ end wb_gpio_08;
 -- Implementation -----------------------------------------------------------
 architecture rtl of wb_gpio_08 is
 
+	constant	OPORT_ADDR : std_ulogic_vector(7 downto 0) := x"04";
+	constant	IPORT_ADDR : std_ulogic_vector(7 downto 0) := x"01";
+
 	signal wbactive  : std_ulogic;
 	
 	signal oport_reg : std_ulogic_vector(7 downto 0);
@@ -38,7 +41,7 @@ begin
 	-- synchronize incoming signals (anti-meta-state)
 	syncproc: process(clk) is
 	begin
-		if clk'event and clk='1' then
+		if (rising_edge(clk)) then
 			iport_reg <= iport;
 		end if;
 	end process;
@@ -48,20 +51,20 @@ begin
 	
 	wb_ack_o <= wb_stb_i and wb_cyc_i;
 	
-	wb_dat_o <= iport_reg when wb_stb_i='1' and wb_cyc_i='1' and wb_adr_i=x"01" else
-				oport_reg when wb_stb_i='1' and wb_cyc_i='1' and wb_adr_i=x"04" else
+	wb_dat_o <= iport_reg when ( wb_stb_i='1' and wb_cyc_i='1' and wb_adr_i=IPORT_ADDR ) else
+				oport_reg when ( wb_stb_i='1' and wb_cyc_i='1' and wb_adr_i=OPORT_ADDR ) else
 				(others => '-');
 	
 	writeproc: process (reset, clk) is
-	variable val : std_ulogic_vector(31 downto 0);
+
 	begin
-		if reset='0' then 
+		if (reset='0') then 
 			oport_reg <= (others => '0');
-		elsif clk'event and clk='1' then
-			if wb_stb_i='1' and wb_cyc_i='1' and wb_we_i='1' then 
+		elsif ( rising_edge(clk)) then
+			if (wb_stb_i='1' and wb_cyc_i='1' and wb_we_i='1') then 
 	
 				-- decode WB_ADR_I --
-				if wb_adr_i=x"04" then 
+				if (wb_adr_i=OPORT_ADDR) then 
 					oport_reg <= wb_dat_i;
 				end if;
 	
